dab7c0ded619ab6e793d2a35913f9fac2220366d
Huy Nguyen <31513571-nguyenchihuy10@users.noreply.replit.com>
2024-05-08 20:45:38 +0000

    new calculator

---
+++ calculator/target/release/calculator.d
@@ -0,0 +1,1 @@
+/home/runner/Rust-In-Replit/calculator/target/release/calculator: /home/runner/Rust-In-Replit/calculator/src/main.rs
---
+++ calculator/target/release/deps/calculator-ae23c4c55f47c666.d
@@ -0,0 +1,5 @@
+/home/runner/Rust-In-Replit/calculator/target/release/deps/calculator-ae23c4c55f47c666: src/main.rs
+
+/home/runner/Rust-In-Replit/calculator/target/release/deps/calculator-ae23c4c55f47c666.d: src/main.rs
+
+src/main.rs:
